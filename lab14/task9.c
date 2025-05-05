42b3e9e1f6640948a804db4292f21516e3e5c44c
Roksana Lozovska <lozovskaaroksana@gmail.com>
2025-05-05 12:49:05 +0200

    Лаб 14

--- lab14/task9.c
+++ lab14/task9.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    char str[] = "Лабораторна робота №14. Виконую 9 варіант";
+    char str[] = "Лабораторна робота №14. Виконую 9 варіант.";
     char result[100]; 
     int i = 0;
     while (str[i] != '.' && str[i] != '\0') {
         result[i] = str[i];
-        i++;
+        i+=1;
     }
     result[i] = '\0'; 
     printf("Підрядок до першої крапки: %s\n", result);
